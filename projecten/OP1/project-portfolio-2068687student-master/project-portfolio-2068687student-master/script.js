7b5814a22ff2cf3bac9df7a7139b60243a77b048
Adjani Valerius <2068687@talnet.nl>
2019-11-05 13:27:36 +0100

    alles van gisteren

---
+++ projecten/OP1/project-portfolio-2068687student-master/project-portfolio-2068687student-master/script.js
@@ -0,0 +1,10 @@
+function verander1() {
+    document.getElementById("selfie1").style.display="none";
+    document.getElementById("selfie2").style.display="block";
+}
+
+function verander2() {
+    document.getElementById("selfie2").style.display="none";
+    document.getElementById("selfie1").style.display="block";
+}
+
